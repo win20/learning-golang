4b1ef71929344a8e2ad009c995d16cce9ff56250
Win Barua <win.barua2@gmail.com>
2025-01-04 15:21:09 +0000

    go-by-example, chap. 29

---
+++ go-by-example/29_channels.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func Channels() {
+	messages := make(chan string)
+
+	go func() { messages <- "ping" }()
+
+	msg := <-messages
+	fmt.Println(msg)
+}
--- go-by-example/main.go
+++ go-by-example/main.go
@@ -28,5 +28,6 @@ func main() {
 	// RangeOverIterators()
 	// Errors()
 	// CustomErrors()
-	Goroutines()
+	// Goroutines()
+	Channels()
 }
